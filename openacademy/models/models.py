0075eb0ff83dc4036b50da4fc5ce3b6725f55acc
toaa62 <toaa62@hotmail.com>
2017-09-05 12:42:18 -0500

    [REF] constraints

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 import time
+from psycopg2 import IntegrityError
 
 def get_uid(self, *a):
     # import pdb; pdb.set_trace()
@@ -19,6 +20,33 @@ class Course(models.Model):
             default=get_uid)
     session_ids = fields.One2many('openacademy.session','course_id')
 
+    _sql_constraints = [
+            ('name_description_check',
+                'CHECK(name != description)',
+                "The title of the course should not be the description"
+
+                ),
+            ('name_unique',
+                'UNIQUE(name)',
+                "the course title must be unique",
+                ),
+            ]
+
+    def copy(self, default=None):
+        if default is None:
+            default = {}
+        copied_count = self.search_count([
+            ('name','ilike','Copy of %s%%' % (self.name))])
+        if not copied_count:
+            new_name = "Copy of %s" % (self.name)
+        else:
+            new_name = "Copy of %s (%s)" % (self.name, copied_count)
+        default['name'] = new_name
+        # default['name'] = self.name + ' otro'
+        # try:
+        return super(Course, self).copy(default)
+        # except IntegrityError:
+        #    import pdb; pdb.set_trace()
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -33,7 +61,7 @@ class Session(models.Model):
     course_id = fields.Many2one('openacademy.course', ondelete='cascade',
                                 string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
-    taken_seats = fields.Float(compute='_taken_seats')
+    taken_seats = fields.Float(compute='_taken_seats', store=True)
     active = fields.Boolean(default=True)
 
     @api.depends('seats', 'attendee_ids')
@@ -44,3 +72,32 @@ class Session(models.Model):
                 record.taken_seats = 0
             else:
                 record.taken_seats = 100.0 * len(record.attendee_ids) / record.seats
+
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seat(self):
+        # if self.seats < 0:
+        if self.filtered(lambda r: r.seats < 0):
+            self.active = False
+            return {
+                    'warning':{
+                        'title': "Incorrect 'seat' value",
+                        'message': "The number of available seats my not be negative",
+                        }
+                    }
+        if self.seats < len(self.attendee_ids):
+            self.active = False
+            return {
+                    'warning':{
+                        'title': "Too many attendees",
+                        'message': "Increase seats or remove excess attendees",
+                        }
+                    }
+        self.active = True
+
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for record in self.filtered('instructor_id'):
+            if record.instructor_id in record.attendee_ids:
+                raise exceptions.ValidationError(
+                        "A session's instructor can't be an attendee"
+                        )
