4a96778a356ca3312ce5ff7aa1308d43a636c2b5
toaa62 <toaa62@hotmail.com>
2017-09-06 12:57:27 -0500

    [REF] openacademy: Security

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -24,10 +24,11 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/partner.xml'
+        'views/partner.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
