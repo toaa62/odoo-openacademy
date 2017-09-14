8398fc0699261c59fa683e3ec9870b9128ec4705
toaa62 <toaa62@hotmail.com>
2017-09-14 12:57:55 -0500

    [REF] Correcciónde errores: __init__.py en models y tests

--- openacademy/tests/__init__.py
+++ openacademy/tests/__init__.py
@@ -1,4 +1,4 @@
 # -*- encoding: utf-8 -*-
 
-# from . import test_global_openacademy_course
-# from . import test_global_openacademy_session
+from . import test_global_openacademy_course
+from . import test_global_openacademy_session
